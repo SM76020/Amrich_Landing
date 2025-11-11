6893e201c721d266be31ee82c14b1932e811df56
Suman Mondal <91243593+SM76020@users.noreply.github.com>
2025-11-11 11:56:03 +0530

    Update db.php

--- config/db.php
+++ config/db.php
@@ -1,9 +1,8 @@
 <?php
-$host = "localhost";
-$dbname = "project_mgmt";
-// $dbname = "amipl";
-$username = "root"; // Change as needed
-$password = "";     // Change as needed
+$host = "sql108.infinityfree.com";
+$dbname = "if0_34626547_project_mgmt";
+$username = "if0_34626547"; // Change as needed
+$password = "YYhqfVxSvoYjP";     // Change as needed
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
